2898ed34695b74bc47708065ac013ac933831f85
Soumyaranjan Parhi <85118185+SoumyaRanjan-1702@users.noreply.github.com>
2023-04-29 14:47:37 +0530

    Firebase1.0_v1

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:telegram_ui/pages/home.dart';
+import 'package:telegram_ui/pages/verify_code.dart';
+import 'package:telegram_ui/pages/round_button.dart';
+import 'package:telegram_ui/pages/utils.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -10,92 +14,64 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool loading = false ;
+  final phoneNumberController = TextEditingController();
+  final auth = FirebaseAuth.instance ;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Telegram'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
           children: [
-            Text(
-              'Login',
-              style: TextStyle(
-                  fontSize: 35,
-                  color: Colors.lightBlueAccent,
-                  fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: Form(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: TextFormField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                        maxLength: 10,
-                        decoration: InputDecoration(
-                          labelText: 'Phone',
-                          hintText: 'Enter Phone Number',
-                          prefixIcon: Icon(Icons.phone_android),
-                          border: OutlineInputBorder(),
-                        ),
-                        onChanged: (String value) {},
-                        validator: (value) {
-                          return value!.isEmpty
-                              ? 'Please enter Phone Number'
-                              : null;
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: TextFormField(
-                        keyboardType: TextInputType.visiblePassword,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          hintText: 'Enter Password',
-                          prefixIcon: Icon(Icons.password),
-                          border: OutlineInputBorder(),
-                        ),
-                        onChanged: (String value) {},
-                        validator: (value) {
-                          return value!.isEmpty
-                              ? 'Please enter Password'
-                              : null;
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 35),
-                      child: MaterialButton(
-                        minWidth: double.infinity,
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => Home()));
-                        },
-                        child: Text('Login'),
-                        color: Colors.lightBlueAccent,
-                        textColor: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
+            SizedBox(height: 80,),
+
+            TextFormField(
+              controller: phoneNumberController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  hintText: '+1 234 3455 234'
               ),
             ),
+            SizedBox(height: 80,),
+            RoundButton(title: 'Login',loading: loading, onTap: (){
+
+              setState(() {
+                loading = true ;
+              });
+              auth.verifyPhoneNumber(
+                  phoneNumber: phoneNumberController.text,
+                  verificationCompleted: (_){
+                    setState(() {
+                      loading = false ;
+                    });
+                  },
+                  verificationFailed: (e){
+                    setState(() {
+                      loading = false ;
+                    });
+                    Utils().toastMessage(e.toString());
+                  },
+                  codeSent: (String verificationId , int? token){
+                    Navigator.push(context,
+                        MaterialPageRoute(
+                            builder: (context) => VerifyCodeScreen(verificationId:verificationId ,)));
+                    setState(() {
+                      loading = false ;
+                    });
+                  },
+                  codeAutoRetrievalTimeout: (e){
+                    Utils().toastMessage(e.toString());
+                    setState(() {
+                      loading = false ;
+                    });
+                  });
+            })
+
           ],
         ),
       ),
